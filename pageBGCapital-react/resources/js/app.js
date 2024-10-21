7b950584038b3f96d3ada07d1b6724911ad288ef
José Manuel Martínez Antonio <josema.k303@gmail.com>
2024-10-21 17:34:34 -0600

    Desactivar Jekyll y ajustar despliegue

--- pageBGCapital-react/resources/js/app.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-import App from './App.jsx'; // Asegúrate de que esta ruta sea correcta
-
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
--- react-bg-capital/src/components/Charts/Charts.styled.ts
+++
@@ -1,6 +0,0 @@
-// Charts.styled.tsx
-import styled from 'styled-components';
-
-export const ChartsWrapper = styled.div`
-  /* Estilos adicionales aquí */
-`;
